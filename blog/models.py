707144f145e778400e9bed0ff9964f52c99e0725
NataTimos <nezhinskaya83@gmail.com>
2021-09-30 16:15:06 +0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    slug = models.SlugField(max_length=250, unique_for_date='published_date')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-published_date',)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.title
