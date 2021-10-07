cf7370ada8ef26e29663f7932da48757ffa1acdd
NataTimos <nezhinskaya83@gmail.com>
2021-10-07 11:05:22 +0300

    add email form

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-
 from .models import Post
 
 class PostForm(forms.ModelForm):
@@ -7,3 +6,12 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, widget=forms.Textarea)
+
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.utils import timezone
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+from .forms import EmailPostForm
 from .models import Post
 from .forms import PostForm
 
-def post_list(request):
-    object_list = Post.objects.filter(status = "published")
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post_list.html', {'page': page, 'posts': posts})
+# def post_list(request):
+#     object_list = Post.objects.filter(status = "published")
+#     paginator = Paginator(object_list, 3)
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post_list.html', {'page': page, 'posts': posts})
+
+class PostListView(ListView):
+    queryset = Post.objects.filter(status = "published")
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/post_list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -24,6 +33,20 @@ def post_detail(request, year, month, day, post):
                                    published_date__month = month,
                                    published_date__day = day)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id = post_id, status = 'published')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.changed_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/share.html', {
+        'post': post,
+        'form': form
+    })
+        
 
 
 # def post_new(request):
